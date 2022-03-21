de0d596573fe686bbbe6374e016449b957961abc
aycadogan89 <doganayca89@gmail.com>
2022-03-21 03:35:29 -0700

    adding routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+const memberRoute = require('./routes/routes')
+
 const app = express()
 app.set('view engine', 'ejs')
 app.set('views','views') //explicitly setting the views directory
@@ -11,6 +13,11 @@ app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use('/api/members',memberRoute)
+
+app.use((req,res) => {
+    res.status(404).send('<h1>Error 404</h1>')
+})
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log(`server started at port ${PORT}`))
