6fa09b494af87a979caa882231a47cbca2fa8d4e
aycadogan89 <doganayca89@gmail.com>
2022-03-21 03:34:00 -0700

    adding routes

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', )
+router.get('/:id')
+router.post('/post')
+router.put('/update/:id')
+router.delete('/delete/:id')
+
+module.exports = router
